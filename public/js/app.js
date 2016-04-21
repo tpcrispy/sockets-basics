74b7f07381d5313cf9c0ff97d8939dfdce2b1e06
Thomas Smith <thomas.1993.smith@gmail.com>
2016-04-21 11:09:47 +1000

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect', function (){
 	console.log('Conncted to a socket.io server!');
 });
+
+socket.on('message', function (message){
+	console.log('New Message');
+	console.log(message.text);
+});
