594d79fa0edca95904dffb97c510a6d0e39cf7d4
Thomas Smith <thomas.1993.smith@gmail.com>
2016-04-21 13:14:33 +1000

    add message to browers UI

--- public/js/app.js
+++ public/js/app.js
@@ -7,13 +7,15 @@ socket.on('connect', function (){
 socket.on('message', function (message){
 	console.log('New Message');
 	console.log(message.text);
+
+	jQuery('.messages').append('<p>' + message.text + '</p>');
 });
 
 
 // Handles submitting of new message
 var $form = jQuery('#message-form');
 
-$form.on('submit', function (){
+$form.on('submit', function (event){
 	event.preventDefault();
 	var $message = $form.find('input[name=message]');
 
@@ -23,5 +25,4 @@ $form.on('submit', function (){
 
 	$message.val('');
 	
-
 });
